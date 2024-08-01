158a54a26c9925c210a97cfa8608681cda4fa0d4
Héctor Soria <78074696+hsoria@users.noreply.github.com>
2024-08-01 18:23:57 +0200

    Update Peptide_calculator.py

--- Peptide_calculator.py
+++ Peptide_calculator.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
 
-def calculate_amino_acid_masses(peptide_sequence, scale, resin_loading, mode, amino_acid_weights):
+def calculate_amino_acid_masses(peptide_sequence, scale, resin_loading, mode, amino_acid_weights, amino_acid_comments):
     Mw_HCTU = 413.69 #g/mol
     Mw_HBTU = 379.247 #g/mol
     Mw_HOBt = 135.12
     Mw_DIEA = 129 #g/mol
     density_DIEA = 0.742 #g/mL
-
-    amino_acid_comments = {aa: f"Fmoc-{aa}" for aa in amino_acid_weights}
 
     if mode == "Temperature":
         amino_acid_masses = {}
@@ -75,35 +73,66 @@ def streamlit_main():
     # Input fields for new amino acid
     new_aa = st.text_input("Enter new amino acid one-letter code")
     new_aa_weight = st.number_input("Enter new amino acid molecular weight", format="%.2f")
-    
-    return sequence, scale, resin_loading, mode, new_aa, new_aa_weight
+    new_aa_comment = st.text_input("Enter new amino acid comment")
+    new_aa_name = st.text_input("Enter new amino acid name")
 
-def display_amino_acid_weights(amino_acid_weights):
-    df = pd.DataFrame({
-        "Amino Acid": amino_acid_weights.keys(),
-        "Molecular weight": amino_acid_weights.values()
-    })
+    # Validation and adding the new amino acid
+    if new_aa and len(new_aa) == 1 and new_aa_weight > 0 and new_aa_comment and new_aa_name:
+        st.success("New amino acid added successfully.")
+    else:
+        st.error("Please enter a valid one-letter code, positive molecular weight, comment, and name.")
+
+    return sequence, scale, resin_loading, mode, new_aa, new_aa_weight, new_aa_comment, new_aa_name
+
+def display_amino_acid_weights(amino_acid_weights, amino_acid_comments, amino_acid_names):
+    amino_acids = list(amino_acid_weights.keys())
+    weights = list(amino_acid_weights.values())
+    comments = [amino_acid_comments[aa] for aa in amino_acid_weights]
+    names = [amino_acid_names[aa] for aa in amino_acid_weights]
+
+    df = pd.DataFrame({"Amino Acid": amino_acids, "Name": names, "Molecular weight": weights, "Comments": comments})
     
     centered_html = f"<div style='width: 100%; text-align: center;'>{df.to_html(index=False)}</div>"
     st.markdown(centered_html, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     amino_acid_weights = {
-        "A": 311.3, "R": 648.8, "N": 354.4, "D": 411.5, "C": 585.7, "c": 391.4,
-        "E": 425.5, "Q": 368.4, "G": 297.3, "H": 619.7, "I": 353.4, "L": 353.4,
-        "K": 468.5, "M": 371.5, "F": 387.4, "P": 337.4, "S": 383.4, "T": 397.5,
-        "W": 526.58, "Y": 459.6, "y": 417.5, "V": 117.15, "Z": 101.07, "J": 178.15,
+        "A": 311.3, "R": 648.8, "N": 354.4, "D": 411.5, "C": 585.7, "c": 391.4, 
+        "E": 425.5, "Q": 368.4, "G": 297.3, "H": 619.7, "I": 353.4, "L": 353.4, 
+        "K": 468.5, "M": 371.5, "F": 387.4, "P": 337.4, "S": 383.4, "T": 397.5, 
+        "W": 526.58, "Y": 459.6, "y": 417.5, "V": 117.15, "Z": 101.07, "J": 178.15, 
         "U": 446.95, "O": 385.42, "o": 381.19
     }
 
-    sequence, scale, resin_loading, mode, new_aa, new_aa_weight = streamlit_main()
+    amino_acid_comments = {
+        "A": "Fmoc-Ala", "R": "Fmoc-Arg(Pbf)", "N": "Fmoc-Asn", "D": "Fmoc-Asp(OtBu)", 
+        "C": "Fmoc-Cys(Trt)", "c": "Fmoc-Cysteic", "E": "Fmoc-Glu(OtBu)", "Q": "Fmoc-Gln", 
+        "G": "Fmoc-Gly", "H": "Fmoc-His(Trt)", "I": "Fmoc-Ile", "L": "Fmoc-Leu", 
+        "K": "Fmoc-Lys(Boc)", "M": "Fmoc-Met", "F": "Fmoc-Phe", "P": "Fmoc-Pro", 
+        "S": "Fmoc-Ser(tBu)", "T": "Fmoc-Thr(tBu)", "W": "Fmoc-Trp(Boc)", "Y": "Fmoc-Tyr(OtBu)", 
+        "y": "Fmoc-Tyr(OMe)", "V": "Fmoc-Val", "Z": "Azido acetic-OH", "J": "Picolyl azide-OH", 
+        "U": "Fmoc-Lys(Me3+Cl-)", "O": "Fmoc-O2Oc-OH", "o": "Fmoc-Aoa-OH"
+    }
 
-    if new_aa and new_aa_weight:
+    amino_acid_names = {
+        "A": "Alanine", "R": "Arginine", "N": "Asparagine", "D": "Aspartic acid", 
+        "C": "Cysteine", "c": "Cysteic acid", "E": "Glutamic acid", "Q": "Glutamine", 
+        "G": "Glycine", "H": "Histidine", "I": "Isoleucine", "L": "Leucine", 
+        "K": "Lysine", "M": "Methionine", "F": "Phenylalanine", "P": "Proline", 
+        "S": "Serine", "T": "Threonine", "W": "Tryptophan", "Y": "Tyrosine", 
+        "y": "Tyrosine methyl ester", "V": "Valine", "Z": "Azido acetic acid", 
+        "J": "Picolyl azide", "U": "Trimethyl lysine", "O": "Fmoc-O2Oc-OH", "o": "Fmoc-Aoa-OH"
+    }
+
+    sequence, scale, resin_loading, mode, new_aa, new_aa_weight, new_aa_comment, new_aa_name = streamlit_main()
+
+    if new_aa and len(new_aa) == 1 and new_aa_weight > 0 and new_aa_comment and new_aa_name:
         amino_acid_weights[new_aa] = new_aa_weight
-    
-    df = calculate_amino_acid_masses(sequence, scale, resin_loading, mode, amino_acid_weights)
-    
+        amino_acid_comments[new_aa] = new_aa_comment
+        amino_acid_names[new_aa] = new_aa_name
+
+    df = calculate_amino_acid_masses(sequence, scale, resin_loading, mode, amino_acid_weights, amino_acid_comments)
     st.write(df)
-    
-    st.title("Amino Acid legend")
-    display_amino_acid_weights(amino_acid_weights)
+
+    st.title("Amino Acid Legend")
+    display_amino_acid_weights(amino_acid_weights, amino_acid_comments, amino_acid_names)
