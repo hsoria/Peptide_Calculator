0ba97c28342b0cd303bde8696e07bd12b1cd4c43
Héctor Soria <78074696+hsoria@users.noreply.github.com>
2024-08-01 18:27:09 +0200

    Update Peptide_calculator.py

--- Peptide_calculator.py
+++ Peptide_calculator.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 
 def calculate_amino_acid_masses(peptide_sequence, scale, resin_loading, mode, amino_acid_weights, amino_acid_comments):
-    Mw_HCTU = 413.69 #g/mol
-    Mw_HBTU = 379.247 #g/mol
+    Mw_HCTU = 413.69  # g/mol
+    Mw_HBTU = 379.247  # g/mol
     Mw_HOBt = 135.12
-    Mw_DIEA = 129 #g/mol
-    density_DIEA = 0.742 #g/mL
+    Mw_DIEA = 129  # g/mol
+    density_DIEA = 0.742  # g/mL
 
     if mode == "Temperature":
         amino_acid_masses = {}
@@ -22,21 +22,21 @@ def calculate_amino_acid_masses(peptide_sequence, scale, resin_loading, mode, am
 
         for amino_acid in amino_acid_masses:
             amino_acid_masses[amino_acid] /= amino_acid_counts[amino_acid]
-            
+
         HCTU_pc = Mw_HCTU * 3 * scale
-        amino_acid_masses["HCTU_per_coupling"] =  HCTU_pc
-        amino_acid_masses["resin_mg"] =  (scale/resin_loading)*1000
+        amino_acid_masses["HCTU_per_coupling"] = HCTU_pc
+        amino_acid_masses["resin_mg"] = (scale / resin_loading) * 1000
         amino_acid_masses["DIEA_per_coupling_µL"] = (scale * 6 * Mw_DIEA / density_DIEA)
 
         df = pd.DataFrame({"Quantities": amino_acid_masses})
         df = round(df, 0)
         df.rename(index=amino_acid_comments, inplace=True)
-        
-    else: 
+
+    else:
         amino_acid_masses = {}
         amino_acid_counts = {}
         for amino_acid in peptide_sequence:
-            if amino_acid not in amino_acid_masses:
+            if (amino_acid not in amino_acid_masses):
                 amino_acid_masses[amino_acid] = 0
                 amino_acid_counts[amino_acid] = 0
             amino_acid_masses[amino_acid] += amino_acid_weights[amino_acid] * scale * 3
@@ -46,19 +46,19 @@ def calculate_amino_acid_masses(peptide_sequence, scale, resin_loading, mode, am
 
         for amino_acid in amino_acid_masses:
             amino_acid_masses[amino_acid] /= amino_acid_counts[amino_acid]
-            
+
         HBTU_pc = Mw_HBTU * 3 * scale
-        amino_acid_masses["HBTU_per_coupling"] =  HBTU_pc
+        amino_acid_masses["HBTU_per_coupling"] = HBTU_pc
 
         HOBt_pc = Mw_HOBt * 3 * scale
-        amino_acid_masses["HOBt_per_coupling"] =  HOBt_pc
+        amino_acid_masses["HOBt_per_coupling"] = HOBt_pc
 
-        amino_acid_masses["resin_mg"] =  (scale/resin_loading)*1000
+        amino_acid_masses["resin_mg"] = (scale / resin_loading) * 1000
         amino_acid_masses["DIEA_per_coupling_µL"] = (scale * 6 * Mw_DIEA / density_DIEA)
         df = pd.DataFrame({"Quantities": amino_acid_masses})
         df = round(df, 0)
         df.rename(index=amino_acid_comments, inplace=True)
-    
+
     return df
 
 def streamlit_main():
@@ -70,17 +70,21 @@ def streamlit_main():
     resin_loading = st.number_input("Enter scale resin loading [mmol/g]", format="%.3f", value=0.74)
     mode = st.selectbox("Synthesis method", ["RT", "Temperature"])
 
-    # Input fields for new amino acid
-    new_aa = st.text_input("Enter new amino acid one-letter code")
-    new_aa_weight = st.number_input("Enter new amino acid molecular weight", format="%.2f")
-    new_aa_comment = st.text_input("Enter new amino acid comment")
-    new_aa_name = st.text_input("Enter new amino acid name")
+    add_new_amino_acid = st.selectbox("Do you want to add a new amino acid?", ["No", "Yes"])
 
-    # Validation and adding the new amino acid
-    if new_aa and len(new_aa) == 1 and new_aa_weight > 0 and new_aa_comment and new_aa_name:
-        st.success("New amino acid added successfully.")
-    else:
-        st.error("Please enter a valid one-letter code, positive molecular weight, comment, and name.")
+    new_aa, new_aa_weight, new_aa_comment, new_aa_name = None, None, None, None
+
+    if add_new_amino_acid == "Yes":
+        new_aa = st.text_input("Enter new amino acid one-letter code")
+        new_aa_weight = st.number_input("Enter new amino acid molecular weight", format="%.2f")
+        new_aa_comment = st.text_input("Enter new amino acid comment")
+        new_aa_name = st.text_input("Enter new amino acid name")
+
+        # Validation and adding the new amino acid
+        if new_aa and len(new_aa) == 1 and new_aa_weight > 0 and new_aa_comment and new_aa_name:
+            st.success("New amino acid added successfully.")
+        else:
+            st.error("Please enter a valid one-letter code, positive molecular weight, comment, and name.")
 
     return sequence, scale, resin_loading, mode, new_aa, new_aa_weight, new_aa_comment, new_aa_name
 
@@ -91,7 +95,7 @@ def display_amino_acid_weights(amino_acid_weights, amino_acid_comments, amino_ac
     names = [amino_acid_names[aa] for aa in amino_acid_weights]
 
     df = pd.DataFrame({"Amino Acid": amino_acids, "Name": names, "Molecular weight": weights, "Comments": comments})
-    
+
     centered_html = f"<div style='width: 100%; text-align: center;'>{df.to_html(index=False)}</div>"
     st.markdown(centered_html, unsafe_allow_html=True)
 
@@ -120,8 +124,8 @@ if __name__ == "__main__":
         "G": "Glycine", "H": "Histidine", "I": "Isoleucine", "L": "Leucine", 
         "K": "Lysine", "M": "Methionine", "F": "Phenylalanine", "P": "Proline", 
         "S": "Serine", "T": "Threonine", "W": "Tryptophan", "Y": "Tyrosine", 
-        "y": "Tyrosine methyl ester", "V": "Valine", "Z": "Azido acetic acid", 
-        "J": "Picolyl azide", "U": "Trimethyl lysine", "O": "Fmoc-O2Oc-OH", "o": "Fmoc-Aoa-OH"
+        "y": "Methyl tyrosine", "V": "Valine", "Z": "Azido acetic acid", "J": "Picolyl azide", 
+        "U": "Trimethyllysine", "O": "Ornithine", "o": "Aminocyclobutanecarboxylic acid"
     }
 
     sequence, scale, resin_loading, mode, new_aa, new_aa_weight, new_aa_comment, new_aa_name = streamlit_main()
